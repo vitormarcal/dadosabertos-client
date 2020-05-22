aa99e1ec3bf300acccbd4a39a199a32b16a159af
Vitor Marçal <vitormarcal8@gmail.com>
2020-05-22 08:24:56 -0300

    update method to remove null values from filter

--- src/main/java/br/com/vitormarcal/dadosabertosclient/deputados/DeputadosWebService.java
+++ src/main/java/br/com/vitormarcal/dadosabertosclient/deputados/DeputadosWebService.java
@@ -2,34 +2,33 @@ package br.com.vitormarcal.dadosabertosclient.deputados;
 
 import br.com.vitormarcal.dadosabertosclient.ExecutorWebClientService;
 import br.com.vitormarcal.dadosabertosclient.result.Result;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NonNull;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public interface DeputadosWebService {
 
     ExecutorWebClientService<Result<List<DeputadoSummary>>> executor = new ExecutorWebClientService<>();
     ObjectMapper mapper = new ObjectMapper();
+    TypeReference<Map<String, Object>> type = new TypeReference<Map<String, Object>>() {};
 
     @GET("v2/deputados/")
     Call<Result<List<DeputadoSummary>>> callDeputadoSummaryList(@QueryMap Map<String, Object> filter);
 
-    @GET("v2/deputados/{id}")
-    Call<ResponseBody> callDeputadoDetail(@Path("id") Integer idDeputado);
-
-    default Result<List<DeputadoSummary>> getDeputadoSummaryList(DeputadosFilter filter) {
-
-        return executor.execute(
-                callDeputadoSummaryList(
-                        mapper.convertValue(filter, Map.class)
-                ));
+    default Result<List<DeputadoSummary>> getDeputadoSummaryList(@NonNull DeputadosFilter filter) {
+        mapper.setDefaultLeniency(true);
+        Map<String, Object> mapFilter = mapper.convertValue(filter, type);
+        mapFilter.values().removeIf(Objects::isNull);
+        return executor.execute(callDeputadoSummaryList(mapFilter));
     }
 
 
